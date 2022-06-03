f9a4ba8f21a2e2812cd7e7bb01e70bbc330c62bd
DonorRaja <srk.raja4@gmail.com>
2022-06-03 16:33:26 +1200

    Unit test implementation.

---
+++ ASBInterviewExerciseTests/Test Types/Extensions/XCTestCase+JSON.swift
@@ -0,0 +1,28 @@
+//
+//  XCTestCase+JSON.swift
+//  ASBInterviewExerciseTests
+//
+//  Created by DonorRaja on 3/06/22.
+//
+
+import XCTest
+
+extension XCTestCase {
+  enum TestError: Error {
+    case fileNotFound
+  }
+  
+  func getData(fromJSON fileName: String) throws -> Data {
+   //let bundle = Bundle(for: type(of: self))
+    guard let url = URL(string: fileName) else {
+      XCTFail("Missing File: \(fileName)")
+      throw TestError.fileNotFound
+    }
+    do {
+      let data = try Data(contentsOf: url)
+      return data
+    } catch {
+      throw error
+    }
+  }
+}
